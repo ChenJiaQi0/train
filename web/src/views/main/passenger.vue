1157fb33df1afbed3240f34ec3b7fecaee5f79c0
TONGTONGDESHIJIE <1090060206@qq.com>
2023-11-21 15:21:13 +0800

    增加乘车人表单

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,14 +1,45 @@
 <template>
   <a-button type="primary" @click="showModal">新增</a-button>
-  <a-modal v-model:visible="visible" title="乘客人" @ok="handleOk">
-    <p>Some content...</p>
-    <p>Some content...</p>
-    <p>Some content...</p>
+  <a-modal
+    v-model:visible="visible"
+    title="乘客人"
+    @ok="handleOk"
+    ok-text="确认"
+    cancel-text="取消"
+  >
+    <a-form
+      :model="passenger"
+      :label-col="{ span: 4 }"
+      :wrapper-col="{ span: 20 }"
+    >
+      <a-form-item label="姓名">
+        <a-input v-model:value="passenger.name" />
+      </a-form-item>
+      <a-form-item label="身份证">
+        <a-input v-model:value="passenger.idCard" />
+      </a-form-item>
+      <a-form-item label="类型">
+        <a-select v-model:value="passenger.type">
+          <a-select-option value="1">成人</a-select-option>
+          <a-select-option value="2">儿童</a-select-option>
+          <a-select-option value="3">学生</a-select-option>
+        </a-select>
+      </a-form-item>
+    </a-form>
   </a-modal>
 </template>
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 const visible = ref(false)
+const passenger = reactive({
+  id: undefined,
+  memeberId: undefined,
+  name: undefined,
+  idCard: undefined,
+  type: undefined,
+  createTime: undefined,
+  updateTime: undefined
+})
 
 const showModal = () => {
   visible.value = true
