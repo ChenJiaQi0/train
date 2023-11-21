af5a5f334ecd16a02c5d10ce1825d0b02fd18b21
TONGTONGDESHIJIE <1090060206@qq.com>
2023-11-21 15:47:52 +0800

    格式化返回的日期字段

--- member/src/main/java/top/chen/train/member/resp/PassengerQueryResp.java
+++ member/src/main/java/top/chen/train/member/resp/PassengerQueryResp.java
@@ -1,5 +1,6 @@
 package top.chen.train.member.resp;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -16,6 +17,8 @@ public class PassengerQueryResp {
     private String name;
     private String idCard;
     private String type;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 }
