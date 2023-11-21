db59421970529cc0534b8d91f03805173b106cca
TONGTONGDESHIJIE <1090060206@qq.com>
2023-11-21 14:58:47 +0800

    修改菜单，实现页面切换

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,19 +1,24 @@
 <template>
   <a-layout-header class="header">
     <div class="logo" />
-      <div style="float: right; color: white">
-        您好：{{ member.mobile }}
-        <router-link to="/login">退出登录</router-link>
-      </div>
+    <div style="float: right; color: white">
+      您好：{{ member.mobile }}
+      <router-link to="/login">退出登录</router-link>
+    </div>
     <a-menu
       v-model:selectedKeys="selectedKeys1"
       theme="dark"
       mode="horizontal"
       :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 1</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome"> <coffee-outlined /> 欢迎 </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <user-outlined /> 乘车人管理
+        </router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
@@ -26,5 +31,4 @@ const member = store.state.member
 const selectedKeys1 = ref(['2'])
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
