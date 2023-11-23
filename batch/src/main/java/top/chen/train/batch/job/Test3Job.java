243dd44d44a250c4d3ebcb423ec5dfcac9c8eb0c
TONGTONGDESHIJIE <1090060206@qq.com>
2023-11-23 13:54:12 +0800

    多节点场景中 quartz 的调度情况

--- batch/src/main/java/top/chen/train/batch/job/Test3Job.java
+++ batch/src/main/java/top/chen/train/batch/job/Test3Job.java
@@ -8,19 +8,13 @@ import org.quartz.JobExecutionException;
 /**
  * @author ChenQi
  * @date 2023/11/23
- * @description: TestJob
+ * @description: Test3Job
  */
 @DisallowConcurrentExecution
-public class TestJob implements Job {
-
+public class Test3Job implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob TEST 开始");
-        //try {
-        //    Thread.sleep(3000);
-        //} catch (InterruptedException e) {
-        //    e.printStackTrace();
-        //}
-        System.out.println("TestJob TEST 结束");
+        System.out.println("TestJob333333 TEST开始");
+        System.out.println("TestJob333333 TEST结束");
     }
 }
