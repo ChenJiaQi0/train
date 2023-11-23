256e113b5c2c8863d420876feabcf3c9bdecc9ef
TONGTONGDESHIJIE <1090060206@qq.com>
2023-11-23 13:45:43 +0800

    通过界面操作定时任务

--- admin/src/components/the-sider.vue
+++ admin/src/components/the-sider.vue
@@ -1,48 +1,63 @@
 <template>
   <a-layout-sider width="200" style="background: #fff">
-    <a-menu v-model:selectedKeys="selectedKeys" mode="inline" :style="{ height: '100%', borderRight: 0 }">
+    <a-menu
+      v-model:selectedKeys="selectedKeys"
+      :openKeys="['batch', 'base']"
+      mode="inline"
+      :style="{ height: '100%', borderRight: 0 }"
+    >
       <a-menu-item key="/welcome">
-        <router-link to="/welcome">
-          <coffee-outlined />
-          欢迎
-        </router-link>
+        <router-link to="/welcome"> <coffee-outlined /> 欢迎 </router-link>
       </a-menu-item>
       <a-menu-item key="/about">
-        <router-link to="/about">
-          <user-outlined />
-          关于
-        </router-link>
+        <router-link to="/about"> <user-outlined /> 关于 </router-link>
       </a-menu-item>
-      <a-menu-item key="/station">
-        <router-link to="/station">
-          <home-outlined />
-          车站管理
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/train">
-        <router-link to="/train">
-          <car-outlined />
-          火车管理
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/train-station">
-        <router-link to="/train-station">
-          <car-outlined />
-          火车车站
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/train-carriage">
-        <router-link to="/train-carriage">
-          <car-outlined />
-          火车车厢
-        </router-link>
-      </a-menu-item>
-      <a-menu-item key="/train-seat">
-        <router-link to="/train-seat">
-          <car-outlined />
-          火车座位
-        </router-link>
-      </a-menu-item>
+      <a-sub-menu key="batch">
+        <template #title>
+          <span>
+            <UnorderedListOutlined />
+            跑批管理
+          </span>
+        </template>
+        <a-menu-item key="/batch/job">
+          <router-link to="/batch/job">
+            <MenuUnfoldOutlined /> 任务管理
+          </router-link>
+        </a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="base">
+        <template #title>
+          <span>
+            <UnorderedListOutlined />
+            基础数据
+          </span>
+        </template>
+        <a-menu-item key="/base/station">
+          <router-link to="/base/station">
+            <car-outlined /> 车站管理
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train">
+          <router-link to="/base/train">
+            <car-outlined /> 火车管理
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train-station">
+          <router-link to="/base/train-station">
+            <car-outlined /> 火车车站
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train-carriage">
+          <router-link to="/base/train-carriage">
+            <car-outlined /> 火车车厢
+          </router-link>
+        </a-menu-item>
+        <a-menu-item key="/base/train-seat">
+          <router-link to="/base/train-seat">
+            <car-outlined /> 火车座位
+          </router-link>
+        </a-menu-item>
+      </a-sub-menu>
     </a-menu>
   </a-layout-sider>
 </template>
@@ -53,12 +68,13 @@ import router from '@/router'
 
 const selectedKeys = ref([])
 
-watch(() => router.currentRoute.value.path, (newValue) => {
-  console.log('watch', newValue)
-  selectedKeys.value = []
-  selectedKeys.value.push(newValue)
-}, { immediate: true })
+watch(
+  () => router.currentRoute.value.path,
+  (newValue) => {
+    console.log('watch', newValue)
+    selectedKeys.value = []
+    selectedKeys.value.push(newValue)
+  },
+  { immediate: true }
+)
 </script>
-
-<style scoped>
-</style>
