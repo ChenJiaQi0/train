9e8cf2135849dc8ae2a4f1a06af4de91c63d291e
TONGTONGDESHIJIE <1090060206@qq.com>
2023-11-26 21:16:39 +0800

    分解选座购票功能的前后端逻辑

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -47,17 +47,17 @@
   <div v-if="tickets.length > 0">
     <a-button type="primary" size="large" @click="finishCheckPassenger">提价订单</a-button>
   </div>
-  <a-modal v-model:visible="visible" title="请核对以下信息" style="top:50px; width: 800px;" ok-text="确认" cancel-text="取消">
+  <a-modal @ok="handleOk" v-model:visible="visible" title="请核对以下信息" style="top:50px; width: 800px;" ok-text="确认" cancel-text="取消">
     <div class="order-tickets">
       <a-row class="order-tickets-header" v-if="tickets.length > 0">
         <a-col :span="3">乘客</a-col>
-        <a-col :span="15">省份证</a-col>
+        <a-col :span="15">身份证</a-col>
         <a-col :span="3">票种</a-col>
         <a-col :span="3">座位类型</a-col>
       </a-row>
       <a-row class="order-tickets-row" v-for="ticket in  tickets" :key="ticket.passengerId">
         <a-col :span="3">{{ ticket.passengerName }}</a-col>
-        <a-col :span="15">{{ ticket.passengeIdCard }}</a-col>
+        <a-col :span="15">{{ ticket.passengerIdCard }}</a-col>
         <a-col :span="3">
           <span v-for="item in PASSENGER_TYPE_ARRAY" :key="item.code">
             <span v-if="item.code === ticket.passengerType">{{ item.desc }}</span>
@@ -71,15 +71,50 @@
           </sapn>
         </a-col>
       </a-row>
+      <br>
+      选座类型chooseSeatType：{{ chooseSeatType }}
+      <!-- <br>
+      选座对象chooseSeatObj：{{ chooseSeatObj }} -->
+      <br />
+      <div v-if="chooseSeatType === 0" style="color: red">
+        您购买的车票不支持选座
+        <div>12306规则：只有全部是一等座或全部是二等座才支持选座</div>
+        <div>12306规则：余票小于一定数量时，不允许选座（本项目以20为例）</div>
+      </div>
+      <div v-else style="text-align: center">
+        <a-switch class="choose-seat-item" v-for="item in SEAT_COL_ARRAY" :key="item.code" v-model:checked="chooseSeatObj[item.code + '1']" :checkedChildren="item.desc" :unCheckedChildren="item.desc" />
+        <div v-if="tickets.length > 1">
+          <a-switch class="choose-seat-item" v-for="item in SEAT_COL_ARRAY" :key="item.code" v-model:checked="chooseSeatObj[item.code + '2']" :checkedChildren="item.desc" :unCheckedChildren="item.desc" />
+        </div>
+        <div style="color: #999">提示：您可以选择{{ tickets.length }}个座位</div>
+      </div>
+      <!-- 座位类型SEAT_COL_ARRAY：{{ SEAT_COL_ARRAY }} -->
+      <br>
+      最终购票：{{ tickets }}
+      最终选座：{{ chooseSeatObj }}
     </div>
   </a-modal>
 </template>
 
 <script setup>
-import { ref, onMounted, watch } from 'vue'
+import { ref, onMounted, watch, computed } from 'vue'
 import axios from 'axios'
 import { notification } from 'ant-design-vue'
 
+const chooseSeatType = ref(0)
+const SEAT_COL_ARRAY = computed(() => {
+  return window.SEAT_COL_ARRAY.filter(item => item.type === chooseSeatType.value)
+})
+const chooseSeatObj = ref({})
+watch(() => SEAT_COL_ARRAY.value, () => {
+  chooseSeatObj.value = {}
+  for (let i = 0; i <= 2; i++) {
+    SEAT_COL_ARRAY.value.forEach((item) => {
+      chooseSeatObj.value[item.code + i] = false
+    })
+  }
+  console.log('初始化两排座位，都是未选中：', chooseSeatObj.value)
+}, { immediate: true })
 const passengers = ref([])
 const passengerOptions = ref([])
 const passengerChecks = ref([])
@@ -152,7 +187,79 @@ const finishCheckPassenger = () => {
     notification.error({ description: '最多只能购买5张车票' })
     return
   }
+  const seatTypesTemp = Tool.copy(seatTypes)
+  for (let i = 0; i < tickets.value.length; i++) {
+    const ticket = tickets.value[i]
+    for (let j = 0; j < seatTypesTemp.length; j++) {
+      const seatType = seatTypesTemp[j]
+      if (ticket.seatTypeCode === seatType.code) {
+        seatType.count--
+        if (seatType.count < 0) {
+          notification.error({ description: seatType.desc + '余票不足' })
+          return
+        }
+      }
+    }
+  }
+  console.log('前端余票检验通过')
+  const ticketSeatTypeCodes = []
+  for (let i = 0; i < tickets.value.length; i++) {
+    const ticket = tickets.value[i]
+    ticketSeatTypeCodes.push(ticket.seatTypeCode)
+  }
+  const ticketSeatTypeCodesSet = Array.from(new Set(ticketSeatTypeCodes))
+  console.log('选好的座位类型：', ticketSeatTypeCodesSet)
+  if (ticketSeatTypeCodesSet.length !== 1) {
+    console.log('选了多种座位，不支持选座')
+    chooseSeatType.value = 0
+  } else {
+    if (ticketSeatTypeCodesSet[0] === SEAT_TYPE.YDZ.code) {
+      console.log('一等座选座')
+      chooseSeatType.value = SEAT_TYPE.YDZ.code
+    } else if (ticketSeatTypeCodesSet[0] === SEAT_TYPE.EDZ.code) {
+      console.log('二等座选座')
+      chooseSeatType.value = SEAT_TYPE.EDZ.code
+    } else {
+      console.log('不是一等座或二等座，不支持选座')
+      chooseSeatType.value = 0
+    }
+    if (chooseSeatType.value !== 0) {
+      for (let i = 0; i < seatTypes.length; i++) {
+        const seatType = seatTypes[i]
+        if (ticketSeatTypeCodesSet[0] === seatType.code) {
+          if (seatType.count < 20) {
+            console.log('余票小于20张就不支持选座')
+            chooseSeatType.value = 0
+            break
+          }
+        }
+      }
+    }
+  }
   visible.value = true
+}
+
+const handleOk = () => {
+  console.log('选好的座位：', chooseSeatObj.value)
+  for (let i = 0; i < tickets.value.length; i++) {
+    tickets.value[i].seat = null
+  }
+  let i = -1
+  for (const key in chooseSeatObj.value) {
+    if (chooseSeatObj.value[key]) {
+      i++
+      if (i > tickets.value.length - 1) {
+        notification.error({ description: '所选座位数大于购票数' })
+        return
+      }
+      tickets.value[i].seat = key
+    }
+  }
+  if (i > -1 && i < (tickets.value.length - 1)) {
+    notification.error({ description: '所选座位数小于购票数' })
+    return
+  }
+  console.log('最终购票：', tickets.value)
 }
 
 onMounted(() => {
@@ -192,5 +299,8 @@ onMounted(() => {
   border-top: none;
   vertical-align: middle;
   line-height: 30px;
+}
+.order-tickets .choose-seat-item {
+  margin: 5px 5px;
 }
 </style>
