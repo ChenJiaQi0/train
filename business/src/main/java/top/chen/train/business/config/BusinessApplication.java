04e39c3b82167ac2cdd99c56bc6a972aeb40402b
TONGTONGDESHIJIE <1090060206@qq.com>
2023-11-28 16:57:09 +0800

    使用 Sentinel 配置限流

--- business/src/main/java/top/chen/train/business/config/BusinessApplication.java
+++ business/src/main/java/top/chen/train/business/config/BusinessApplication.java
@@ -1,8 +1,5 @@
 package top.chen.train.business.config;
 
-import com.alibaba.csp.sentinel.slots.block.RuleConstant;
-import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
-import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +9,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-
-import java.util.ArrayList;
 
 /**
  * @author ChenQi
@@ -35,17 +30,17 @@ public class BusinessApplication {
         LOG.info("测试地址：\thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
 
         // 限流规则
-        initFlowRules();
-        LOG.info("已定义限流规则");
+//        initFlowRules();
+//        LOG.info("已定义限流规则");
     }
 
-    private static void initFlowRules() {
-        ArrayList<FlowRule> rules = new ArrayList<>();
-        FlowRule rule = new FlowRule();
-        rule.setResource("doConfirm");
-        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
-        rule.setCount(1);
-        rules.add(rule);
-        FlowRuleManager.loadRules(rules);
-    }
+//    private static void initFlowRules() {
+//        ArrayList<FlowRule> rules = new ArrayList<>();
+//        FlowRule rule = new FlowRule();
+//        rule.setResource("doConfirm");
+//        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+//        rule.setCount(1);
+//        rules.add(rule);
+//        FlowRuleManager.loadRules(rules);
+//    }
 }
