3fccc27e31055afdf441197773881ae1f53c7768
TONGTONGDESHIJIE <1090060206@qq.com>
2023-12-01 14:55:11 +0800

    界面显示优化

--- web/src/views/main/welcome.vue
+++ web/src/views/main/welcome.vue
@@ -1,7 +1,51 @@
 <template>
-  <h1>欢迎使用 12306 售票系统</h1>
+  <div style="border: 4px solid #99CCFF;
+              border-radius: 4px;
+              padding: 10px; text-align: left">
+    <h3>
+      >欢迎使用 12306售票系统
+    </h3>
+    本项目共有15张业务表，自制通用代码生成器，快速生成增删改查包含界面。<br>
+    购票流程看起来简单，但用了很多看不见的高并发技术，比如10万人抢1000张票：<br>
+    <li>利用<b>分布式缓存</b>，在秒杀开始前，提供高性能余票查询，同时要考虑缓存击穿、穿透、雪崩等问题</li>
+    <li>使用<b>第一层验证码</b>，削弱瞬时高峰，将100毫秒内10万人的请求，分散成1~2秒内10万人请求</li>
+    <li>使用<b>第二层验证码</b>，后端验证码，进一步分散请求，同时防止机器人抢票</li>
+    <li>使用<b>限流技术</b>减轻无谓请求，同时给用户快速失败响应（告知票没有了），将9万请求快速失败，变成1万请求抢1000张票
+    </li>
+    <li>使用<b>令牌发放技术</b>，控制抢票量，同时防止机器人刷票，比如开放2000令牌，即变成8000请求快速失败，变成2000请求抢1000张票
+    </li>
+    <li>使用<b>分布式锁技术</b>，防止超卖，即2000人抢1000张票，最终只能卖出1000张，不能卖出1001张票</li>
+    <li>使用<b>异步削峰+排队机制</b>，解决吞吐量问题，实现最短时间内给用户反馈，1000请求告知票没有了，1000请求告知排队中
+    </li>
+    <li>使用<b>分布式事务</b>，保证数据最终一致性，不能库存减少了，票却没打出来。</li>
+  </div>
+  <a-divider style="border-color: #7cb305" dashed />
+  <div style="border: 4px solid #99CCFF;
+              border-radius: 4px;
+              padding: 10px; text-align: left">
+    <div>
+      如果还没有乘车人，可以先去
+      <router-link to="/passenger">
+        添加乘车人
+      </router-link>
+    </div>
+    <div>
+      购票功能支持选座，去
+      <router-link to="/ticket">
+        查询余票并购票
+      </router-link>
+      。购票成功后，可以查看
+      <router-link to="/my-ticket">
+        我的车票
+      </router-link>
+      ；去
+      <router-link to="/seat">
+        座位销售图
+      </router-link>
+      查看每个座位的销售情况
+    </div>
+    <div style="font-size: 14px; color: red">
+      本项目还有一套控台管理模块，用于维护车次数据、调度任务，查看会员、车票等功能
+    </div>
+  </div>
 </template>
-
-<script setup></script>
-
-<style scoped></style>
